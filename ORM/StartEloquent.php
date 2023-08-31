c79fc4cba58134ab3eee08a2246aa033e5665a67
amir aghighi <aghighiamir@yahoo.com>
2023-08-31 22:16:08 +0330

    feat: optimize orm files

--- ORM/StartEloquent.php
+++ ORM/StartEloquent.php
@@ -1,26 +1,19 @@
 <?php
-
-require 'vendor/autoload.php';
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-$config = require 'config.php';
-
+$config = [
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'eloquent',
+            'username' => 'root',
+            'password' => ''
+        ]
+    ]
+];
 
 $capsule = new Capsule;
-$capsule->addConnection($config['connections']['mysql'], 'temp');
+$capsule->addConnection($config['connections']['mysql']);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
-
-createDatabase($config['database']);
-
-
-function createDatabase(string $database)
-{
-    global $capsule, $config;
-    $config['connections']['mysql']['database'] = $database;
-    $capsule->getConnection('temp')->statement("create DATABASE if not EXISTS $database;");
-    $capsule->getConnection('temp')->disconnect();
-    $capsule->addConnection($config['connections']['mysql']);
-}
-
